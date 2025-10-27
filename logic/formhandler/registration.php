13e713602590ca2fbaa39d4a4929ba94f88b7613
Dominik Sommer <wi24b118@technikum-wien.at>
2025-10-27 16:42:40 +0100

    Initial commit

---
+++ logic/formhandler/registration.php
@@ -0,0 +1,6 @@
+<?php
+$firstname = $_POST["firstname"] ?? "";
+$lastname  = $_POST["lastname"] ?? "";
+var_dump($_POST);
+echo "Firstname: {$_POST["firstname"]}";
+?>
---
+++ logic/formhandler/registration_form.php
@@ -0,0 +1,9 @@
+<?php
+
+$email = $_POST["email"];
+$accept = isset($_POST["accept"]);
+
+var_dump($accept);
+
+echo $email . " " . $accept;o
+
